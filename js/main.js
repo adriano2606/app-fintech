f608b7949ad78ac7e4594c9d0e6228a377b114f4
Adriano de Camargo <adrianodecamargo123@gmail.com>
2024-06-26 08:53:38 -0300

    Criação do greeting, botões e saldo disponível

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const navMenu = document.getElementsByTagName('nav')[0]
+const navItems = Array.from(navMenu.getElementsByTagName('i'))
+
+navItems.forEach(element => {
+    element.addEventListener('click', () => {
+        const itemSelected = document.getElementsByClassName('item-menu-selected')[0]
+        if (!element.classList.contains('item-menu-selected')){
+            element.classList.add('item-menu-selected')
+            itemSelected.classList.remove('item-menu-selected')
+        }
+    })
+});
